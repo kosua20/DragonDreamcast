60dfe7310450e6a849cc9ee92b16b8499acd01bd
Simon Rodriguez <kosua20@gmail.com>
2025-07-07 01:04:21 +0200

    Add skybox face.

--- src/data.h
+++ src/data.h
@@ -1,9 +1,9 @@
 #ifndef DATA_HEADER
 #define DATA_HEADER
 
-#include "data/cube_data.h"
 #include "data/plane_data.h"
 #include "data/dragon_data.h"
 #include "data/monkey_data.h"
+#include "data/sky_side_data.h"
 
 #endif
---
+++ src/data/sky_side_data.h
@@ -0,0 +1,30 @@
+const uint16_t points_count_sky_side = 6;
+
+const uint16_t vertex_count_sky_side = 4;
+
+uint16_t points_sky_side[6]  __attribute__((aligned(32)))= {
+	0, 1, 2, 
+	2, 3, 0
+};
+
+vec3f_t vertices_sky_side[4]  __attribute__((aligned(32)))= {
+	{.x= -10.0000, .y=10.0000, .z=0.0000},
+	{.x= 10.0000, .y=10.0000, .z=0.0000},
+	{.x= 10.0000, .y=-10.0000, .z=0.0000},
+	{.x= -10.0000, .y=-10.0000, .z=0.0000},
+};
+
+vec3f_t normals_sky_side[4] = {
+	{.x=0.0000, .y=0.0000, .z=1.0000},
+	{.x=0.0000, .y=0.0000, .z=1.0000},
+	{.x=0.0000, .y=0.0000, .z=1.0000},
+	{.x=0.0000, .y=0.0000, .z=1.0000},
+};
+
+float texcoords_sky_side[8] = {
+	1.0000, 0.0000,
+	0.0000, 0.0000,
+	0.0000, 1.0000,
+	1.0000, 1.0000,
+};
+
